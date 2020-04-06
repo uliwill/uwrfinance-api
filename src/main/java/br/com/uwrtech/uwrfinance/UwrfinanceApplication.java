c9cf5b41f8c14cd50e131f17e8d32610107f3b05
Ulisses Willian Reichenbach <uliwill@gmail.com>
2020-04-06 18:40:42 -0300

    Adicionado CORS.

--- src/main/java/br/com/uwrtech/uwrfinance/UwrfinanceApplication.java
+++ src/main/java/br/com/uwrtech/uwrfinance/UwrfinanceApplication.java
@@ -2,10 +2,21 @@ package br.com.uwrtech.uwrfinance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class UwrfinanceApplication {
+@EnableWebMvc
+public class UwrfinanceApplication implements WebMvcConfigurer{
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// TODO Auto-generated method stub
+		//WebMvcConfigurer.super.addCorsMappings(registry);
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UwrfinanceApplication.class, args);
 	}
